3a47210a6defd312654fd9d3a8b146de4534e906
jinyr1128 <jinyr1128@gmail.com>
2023-12-05 12:09:03 +0900

    first commit : start Project

---
+++ src/main/java/com/tmproject/TmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmProjectApplication.class, args);
+    }
+
+}
