81930d9d98520b0fca17ed0332362332b71e8a60
jinyr1128 <jinyr1128@gmail.com>
2023-12-07 21:57:00 +0900

    fix : onclick && Images && oauthLogin

--- src/main/java/com/tmproject/homeController.java
+++ src/main/java/com/tmproject/homeController.java
@@ -1,11 +1,13 @@
 package com.tmproject;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class homeController {
 
     @GetMapping("/api/member/loginPage")
     public String tmpLoginPage(){
-        return "index";
+        return "oauthLogin";
     }
 }
