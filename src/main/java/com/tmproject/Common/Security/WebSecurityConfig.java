db451f9bf730fcb057829206c34e2e89418ea2ec
mankcom3799@hufs.ac.kr <mankcom3799@hufs.ac.krecho>
2023-12-08 16:32:53 +0900

    #fix websecurityConfig 임시 수정

--- src/main/java/com/tmproject/Common/Security/WebSecurityConfig.java
+++ src/main/java/com/tmproject/Common/Security/WebSecurityConfig.java
@@ -62,12 +62,12 @@ public class WebSecurityConfig {
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                         .requestMatchers("/api/**").permitAll()
-                        .requestMatchers("/api/member/login").permitAll()
+                        /*.requestMatchers("/api/member/login").permitAll()
                         .requestMatchers("/api/member/signup").permitAll()
                         .requestMatchers("/api/member/loginPage").permitAll()
                         .requestMatchers("/api/member/kakao/callback").permitAll()
                         .requestMatchers("/api/member/naver/callback").permitAll()
-                        .requestMatchers("/api/member/google/callback").permitAll()
+                        .requestMatchers("/api/member/google/callback").permitAll()*/
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
